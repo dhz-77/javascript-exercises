bccdd55067720898f9b4ba14b3265c0ae095f51d
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-04 23:46:03 +0200

    Create functions and pass test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
+const add = function(...args) {
+  let sum = 0;
+	for (number of args) {
+    sum += number;
+  }
+  return sum;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function([...args]) {
+	let sum = 0;
+	for (number of args) {
+    sum += number;
+  }
+  return sum;
+};
+
+const multiply = function([...args]) {
+  let product = 1;
+	for (number of args) {
+    product *= number;
+  }
+  return product;
+};
+
+const power = function(num1, num2) {
+	return num1 ** num2;
+};
+
+const factorial = function(num) {
+
+  let factorial = 1;
+
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+// see model solution for shorter functions
 
 // Do not edit below this line
 module.exports = {
