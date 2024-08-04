a6a2bd5a7091a92379d17398a28190350e047e44
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-04 14:40:35 +0200

    Create function and pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+
+    for (number of numbers) {
+        for (let i = array.length - 1; i >= 0; i--) { // reverse loop necessary to delete multiple of the same array values
+            if (array[i] === number) {
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
 };
+
+// see model solution for alternative, shorter solutions
 
 // Do not edit below this line
 module.exports = removeFromArray;
