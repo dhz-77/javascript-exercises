a17388d0e7e4e31d37d6980b314f8c046373c2ec
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-05 12:49:20 +0200

    Create function and pass test
    The algorithm created works flawlessly, less verbose than the model
    solution.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
 
+    let arr = [0, 1]; // -> array[0] = 0, array[1] = 1
+    for (let i = 1; i < number; i++) {
+        arr.push(arr[i-1] + arr[i]) // -> array[2] = 1 
+    }
+    return arr[number];
 };
 
 // Do not edit below this line
