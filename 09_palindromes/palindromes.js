217b8aecda6fbb4f07e41e6aab7ecaa5797b40e8
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-05 12:06:59 +0200

    Create function and pass test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (words) {
 
+    let arr = words.toLowerCase().split("")
+
+    const filteredArray = arr.filter(e => e !== "!" && e !== " " && e !== "." &&  e !== ",")
+    // equals: let filteredArray = arr.filter(function(e) { return e !== "!" .... })
+    const temp = filteredArray.slice(0); // make a new copy of the array, bc without "slice" method, only a reference to the same array is made, and it will not be protected from changes
+    const reversedArray = temp.reverse();
+
+    if (reversedArray.toString() === filteredArray.toString()) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
