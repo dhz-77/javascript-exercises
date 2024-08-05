141e2aa0aa99682163b994666c83a42e5fc2ed46
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-05 13:46:24 +0200

    Create function and pass test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const arr = [];
+    array.forEach((obj) => arr.push(obj["title"]));
+    return arr;
 };
+
+// see model solution for alternative function with array.map
 
 // Do not edit below this line
 module.exports = getTheTitles;
